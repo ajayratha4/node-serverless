69d2300fc2c240197430faca0cd6a2228394cdfe
ajayratha4 <ajayratha4@gmail.com>
2022-12-06 23:26:57 +0530

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+
+const express = require("express");
+const sls = require("serverless-http");
+const app = express();
+app.get("/", async (req, res, next) => {
+  console.log("hello");
+  res.status(200).send("Hello World!");
+});
+module.exports.server = sls(app);
